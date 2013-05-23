3595e404d1ba804c2fafa6de89259d36851045a9
vagrant <vagrant@precise64.(none)>
2013-05-23 18:16:41 +0000

    First commit. All RoR project added.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YakissWebsite::Application.config.secret_token = '42d5b5a465cea8b48565431f751ecd5087ac33708dc894f1c55528cfca245dbbafb4efe6cfe9735f1528350591303f43c32f73de224272b88fd6f624104f9ba6'
