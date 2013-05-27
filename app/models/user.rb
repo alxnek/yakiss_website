dc3fe49e631b791a6719e2ed10cde1146b017b97
Rubén Jiménez <rubenjimenez@ismuser.com>
2013-05-27 21:10:07 +0200

    Finished signup part. Users now can register themselves into YAKISS project.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
 
   has_secure_password
   validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 6 }
 
   before_save { self.email = email.downcase }
 
